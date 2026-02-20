40a91666b1260602d1bd44bf797ca7a6397000e8
Jonh Wili Carvalho <jonhwili@hotmail.com>
2026-02-20 15:10:35 -0300

    curriculum archieve and contact email correction

--- src/data/social.ts
+++ src/data/social.ts
@@ -1,7 +1,7 @@
 export const social = {
   linkedin: "https://linkedin.com/in/jonhwili",
   github: "https://github.com/jonhwili",
-  email: "contact@jonhwili.dev",
+  email: "jonhwili@hotmail.com",
   location: "Brazil",
   domain: "jonhwili.dev",
 };
